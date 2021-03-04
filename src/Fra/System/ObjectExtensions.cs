4bc6507e6585e07c019c142152859a9762da892f
不正经码农 <huohaihang@126.com>
2021-03-04 18:10:26 +0800

    初始化项目

---
+++ src/Fra/System/ObjectExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class ObjectExtensions
+    {
+
+        public static T CheckNotNull<T>(this T source, string parameterName, string message)
+            where T : class
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(parameterName, message);
+            }
+
+            return source;
+        }
+
+        public static T CheckNotNull<T>(this T source, string parameterName)
+            where T : class
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return source;
+        }
+    }
+}
