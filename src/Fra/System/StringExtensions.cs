d6fa7a1895706db1ce3fff19e5ea65bc547fe157
不正经码农 <huohaihang@126.com>
2021-03-08 17:14:35 +0800

    扩展分方法

--- src/Fra/System/StringExtensions.cs
+++ src/Fra/System/StringExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,80 @@ namespace System
         public static bool IsNullOrEmpty(this string source)
         {
             return string.IsNullOrEmpty(source);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string source)
+        {
+            return string.IsNullOrWhiteSpace(source);
+        }
+
+        public static string CheckNotNullOrEmpty(this string source, string parameterName)
+        {
+            if (source.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return source;
+        }
+
+        public static string CheckNotNullOrWhiteSpace(this string source, string parameterName)
+        {
+            if (source.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentException(parameterName);
+            }
+
+            return source;
+        }
+
+        public static T ToEnum<T>(this string source, bool ignoreCase)
+            where T : Enum
+        {
+            source.CheckNotNullOrEmpty(nameof(source));
+
+            return (T)Enum.Parse(typeof(T), source, ignoreCase);
+        }
+
+        public static T ToEnum<T>(this string source)
+            where T : Enum
+        {
+            source.CheckNotNullOrEmpty(nameof(source));
+
+            return (T)Enum.Parse(typeof(T), source);
+        }
+
+        public static byte[] GetBytes(this string source, Encoding encoding)
+        {
+            source.CheckNotNullOrEmpty(nameof(source));
+            encoding.CheckNotNull(nameof(encoding));
+
+            return encoding.GetBytes(source);
+        }
+
+        public static byte[] GetBytes(this string source)
+        {
+            return source.GetBytes(Encoding.UTF8);
+        }
+
+        public static string ToMd5(this string source)
+        {
+            source.CheckNotNullOrEmpty(nameof(source));
+
+            using (var md5 = MD5.Create())
+            {
+                byte[] sourceBytes = Encoding.UTF8.GetBytes(source);
+                byte[] hashBytes = md5.ComputeHash(sourceBytes);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var b in hashBytes)
+                {
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
         }
     }
 }
