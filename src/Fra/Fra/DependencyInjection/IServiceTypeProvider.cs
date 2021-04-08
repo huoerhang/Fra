4bbf2ba29e14d33cc81da8e331bff9f0eedbbce7
不正经码农 <huohaihang@126.com>
2021-04-08 13:26:35 +0800

    删除无用类

--- src/Fra/Fra/DependencyInjection/IServiceTypeProvider.cs
+++ src/Fra/Fra/DependencyInjection/IServiceTypeProvider.cs
@@ -5,7 +5,5 @@ namespace Fra.DependencyInjection
     public interface IServiceTypeProvider
     {
         ServiceTypeDescriptor GetServiceTypeDescriptor(Type implementationType);
-
-        //Type[] GetServiceTypes(Type targetType);
     }
 }
