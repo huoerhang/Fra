9a4e7d51bb4f8e87f1026541db5b4365ccb6f163
huohaihang <huohaihang@126.com>
2021-04-10 00:36:33 +0800

    Modularity

---
+++ src/Fra/Fra/Modularity/AppModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fra.Modularity
+{
+    public abstract class AppModule : IAppModule
+    {
+        private ServiceConfigurationContext _serviceConfigurationContext;
+
+        protected internal ServiceConfigurationContext ServiceConfigurationContext
+        {
+            get
+            {
+                if (_serviceConfigurationContext == null)
+                {
+                    throw new Exception($"The {nameof(ServiceConfigurationContext)} is only available in the {nameof(ConfigureServices)}, {nameof(PreConfigureServices)} and {nameof(PostConfigureServices)} methods.");
+                }
+
+                return _serviceConfigurationContext;
+            }
+            internal set => _serviceConfigurationContext = value;
+        }
+
+        public virtual void ConfigureServices(ServiceConfigurationContext context)
+        {
+
+        }
+
+        public virtual void PostConfigureServices(ServiceConfigurationContext context)
+        {
+
+        }
+
+        public virtual void PreConfigureServices(ServiceConfigurationContext context)
+        {
+
+        }
+    }
+}
---
+++ src/Fra/Fra/Modularity/ServiceConfigurationContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fra.Modularity
+{
+    public class ServiceConfigurationContext
+    {
+        public ServiceConfigurationContext(IServiceCollection services)
+        {
+            Services = services;
+            Items = new Dictionary<string, object>();
+        }
+
+        public IServiceCollection Services { get; }
+
+        public IDictionary<string, object> Items { get; }
+
+        public object this[string key]
+        {
+            get => Items.GetOrDefault(key);
+            set => Items[key] = value;
+        }
+    }
+}
