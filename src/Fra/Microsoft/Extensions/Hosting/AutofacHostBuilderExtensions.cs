d2f55497e4654f64baa9a3b98168c6670fd8e56e
CV Developer <huohaihang@126.com>
2021-04-21 16:48:14 +0800

    Autofac

---
+++ src/Fra/Microsoft/Extensions/Hosting/AutofacHostBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Builder;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class AutofacHostBuilderExtensions
+    {
+        public static IHostBuilder UserAutofac(this IHostBuilder hostBuilder)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            return hostBuilder.ConfigureServices((_, services) =>
+             {
+                 services.AddObjectAccessor(containerBuilder);
+             }).UseServiceProviderFactory(new AutofacServiceProviderFactory());
+        }
+    }
+}
